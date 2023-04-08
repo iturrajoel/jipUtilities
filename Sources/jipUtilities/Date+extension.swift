61c5a6a8d158ec1ec6c1b88df8c8314238e34076
Joel A. Iturra <joel@iturra.nyc>
2023-04-08 14:42:49 -0400

    Initial commit.
    Includes an extension for Date and the WeeklyDateSelector view.

---
+++ Sources/jipUtilities/Date+extension.swift
@@ -0,0 +1,53 @@
+//
+//  Date+extension.swift
+//  ForGitHub
+//
+//  Created by Joel Iturra on 3/29/23.
+//
+
+import Foundation
+
+
+
+extension Date {
+    
+    
+    
+    /// Returns the first day of the week
+    ///
+    /// For example, for New York if the date is Mar 29, 2023 07:54:00 -0500, it will return Mar 26, 2023 07:54:00 -0500
+    var firstDayOfWeek: Date {
+        let weekday = Calendar.current.dateComponents([.weekday], from: self).weekday!
+        return Calendar.current.date(byAdding: .day, value: -weekday + 1, to: self)!
+    }
+    
+    
+    
+    /// Returns the first hour of the day
+    ///
+    /// For example, for New York if the date is Mar 29, 2023 07:54:00 -5000, it will return Mar 29, 2023 00:00:00 -0500
+    var begginingOfDay: Date {
+        let components = Calendar.current.dateComponents([.day, .month, .year, .calendar, .era], from: self)
+        return Calendar.current.date(from: components)!
+    }
+    
+    
+    
+    /// Returns the first hour of the first day of the week
+    ///
+    /// For example, for New York if the date is Mar 29, 2023 07:54:00 0-0500, it will return Mar 26, 2023 00:00:00 -0500
+    var begginingOfThisWeek: Date {
+        firstDayOfWeek.begginingOfDay
+    }
+    
+    
+    /// Returns the first hour of the first day of the next week
+    ///
+    /// For example, for New York if the date is Mar 29, 2023 07:54:00 -0500, it will return Apr 2, 2023 00:00:00 -0500
+    var beggningOfNextWeek: Date {
+        Calendar.current.date(byAdding: .day, value: 7, to: self)!.begginingOfThisWeek
+    }
+    
+    
+    
+}
---
+++ Sources/jipUtilities/WeekDateSelector.swift
@@ -0,0 +1,65 @@
+//
+//  WeeklyDateSelector.swift
+//  ForGitHub
+//
+//  Created by Joel Iturra on 3/29/23.
+//
+
+import SwiftUI
+
+
+/// Date selector that select the entire week when selecting any of the day in the desired week.
+///
+/// For example, if the user selectes Mar 21, 2023, the entire week from Mar 19 to Mar 25 will be selected automatically.
+///
+/// This depends in the current calendar (timezone and locale), however, it has not been tested in other time zones and locales.
+@available(iOS 16.0, *)
+struct WeekDateSelector: View {
+    
+    @Binding private var selection: Set<DateComponents>
+    private let range: Range<Date>
+    
+    
+    
+    init(selection: Binding<Set<DateComponents>>, range: Range<Date> = Date.distantPast..<Date.distantFuture) {
+        _selection = selection
+        self.range = range
+    }
+    
+    
+    
+    var body: some View {
+        MultiDatePicker("", selection: $selection, in: range)
+            .onChange(of: selection) { [selection] newSelection in
+                
+                // First check if we need to recalculate the selection.
+                guard newSelection.count > 0, selection != newSelection else { return }
+                
+                // If there is a new added date, use the first to calculate the new week
+                if let firstAdded = newSelection.subtracting(selection).first {
+                    
+                    // Calculate the first hour of the week and the first hour of the next week
+                    let day0 = Calendar.current.date(from: firstAdded)!.begginingOfThisWeek
+                    let day7 = day0.beggningOfNextWeek
+                    
+                    var weekSelection = Set<DateComponents>()
+                    var date = day0
+                    
+                    // Add the components for each day between day0 and day7 (day0..<day7)
+                    while date.compare(day7) == .orderedAscending {
+                        weekSelection.insert(Calendar.current.dateComponents([.calendar, .era, .day, .month, .year], from: date))
+                        date = Calendar.current.date(byAdding: .day, value: 1, to: date)!
+                    }
+                    
+                    self.selection = weekSelection
+                    
+                } else {
+                    // otherwise, remove all the week, because it's a deselection
+                    self.selection.removeAll()
+                }
+            }
+    }
+    
+    
+    
+}
